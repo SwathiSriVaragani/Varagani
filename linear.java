d7d7bb0dcff7370489332e713fe24ada4b6f3159
SwathiSriVaragani <153272363+SwathiSriVaragani@users.noreply.github.com>
2023-12-14 16:19:10 +0530

    Update linear.java

--- linear.java
+++ linear.java
@@ -1,4 +1,4 @@
-public class LinearSearch{    
+public class linear{    
 public static int linearSearch(int[] arr, int key){    
         for(int i=0;i<arr.length;i++){    
             if(arr[i] == key){    
